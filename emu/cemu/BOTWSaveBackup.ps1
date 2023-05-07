4a5e9c4b81b97437d86b225cc0c6dec14f15ff35
unknown <George@homelab.local>
2023-05-07 21:05:49 +0100

    Cemu botw save script added

---
+++ emu/cemu/BOTWSaveBackup.ps1
@@ -0,0 +1,4 @@
+﻿robocopy /e E:\Wiiu\mlc\usr\save\00050000\101c9500 C:\Users\george\Desktop\101c9500
+
+Compress-Archive -Path C:\Users\george\Desktop\101c9500 -DestinationPath "C:\Users\george\Desktop\BOTWSave - $(get-date -f yyyy-MM-dd).zip"
+
