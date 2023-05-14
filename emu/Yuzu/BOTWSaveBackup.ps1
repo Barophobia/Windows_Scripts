16fbac3627b2017ef70fc7ca135f3880df876eb6
barophobia <barophobia@Apollo.local>
2023-05-14 19:16:03 +0100

    Added yuzu backup script

---
+++ emu/Yuzu/BOTWSaveBackup.ps1
@@ -0,0 +1,4 @@
+﻿robocopy /e E:\Wiiu\mlc\usr\save\00050000\101c9500 C:\Users\barophobia\Desktop\101c9500
+
+Compress-Archive -Path C:\Users\barophobia\Desktop\101c9500 -DestinationPath "C:\Users\barophobia\Desktop\BOTWSave - $(get-date -f yyyy-MM-dd).zip"
+
---
+++ emu/Yuzu/TOTKSaveBackup.ps1
@@ -0,0 +1,4 @@
+﻿robocopy /e C:\Users\barophobia\AppData\Roaming\yuzu\nand\user\save\0000000000000000\C3DC46EFDF90A5B8286AF7AEBEAEFEEE C:\Users\barophobia\Desktop\
+
+Compress-Archive -Path C:\Users\barophobia\Desktop\0100F2C0115B6000 -DestinationPath "C:\Users\barophobia\Desktop\TOTKSave - $(get-date -f yyyy-MM-dd).zip"
+
